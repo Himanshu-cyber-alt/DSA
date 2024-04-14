f20f0ef72e610b8362dcedbc10f24e141d118d25
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2024-04-14 15:23:20 +0530

    subtraction is done

---
+++ IdeaProjects/Github/src/sub.java
@@ -0,0 +1,8 @@
+public class sub {
+    public static void main(String[] args) {
+        int a = 54;;
+        int b = 34;
+        System.out.println("sum of two number "+ (a-b));
+        System.out.println(a-b);
+    }
+}
